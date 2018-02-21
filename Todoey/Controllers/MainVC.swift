e7d469b2cb50a40afe16a407a71c6f6c1abcdceb
MehdiChennoufi <m.chennoufi@me.com>
2018-02-21 15:12:07 +0100

    - Added MVC - Modified save and load's methods items to a local .plist file instead of UserDefaults

--- Todoey/Controllers/MainVC.swift
+++ Todoey/Controllers/MainVC.swift
@@ -9,74 +9,102 @@
 import UIKit
 
 class MainVC: UITableViewController {
-
-  // MARK: - Variables
-  let defaults = UserDefaults.standard
-  var itemArray : [String] = []
-  
+    
+    // MARK: - Variables
+    
+    var itemArray : [Item] = []
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-      if let items = defaults.array(forKey: "todoListArray") as? [String] {
-        itemArray = items
-      }
-      
+        
+        loadItems()
+        
     }
-
-  // MARK: - Table View Data Source Methods
-
+    
+    // MARK: - Table View Data Methods
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return itemArray.count
     }
-
-  
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "todoCell", for: indexPath)
-
-         cell.textLabel?.text = itemArray[indexPath.row]
-
+        
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = itemArray[indexPath.row].title
+        
+        item.done ? (cell.accessoryType = .checkmark) : (cell.accessoryType = .none)
+        
         return cell
     }
- 
-    //MARK: Table View Delegate Methods
-
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-      
-      if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark  {
-          tableView.cellForRow(at: indexPath)?.accessoryType = .none
-      } else {
-          tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-      }
-      
-        tableView.deselectRow(at: indexPath, animated: true)
-    }
-  
-    //MARK - Actions
-  
-    @IBAction func addItemButtonTapped(_ sender: UIBarButtonItem) {
-      var textField = UITextField()
-      
-      let alert = UIAlertController(title: "Add new Todoey Item", message: "", preferredStyle: .alert)
-      let alertAction = UIAlertAction(title: "Add Item", style: .default) { (action) in
         
-      guard textField.text?.isEmpty == false else {
-        return
-      }
-        self.itemArray.append(textField.text!)
-        self.defaults.set(self.itemArray, forKey: "todoListArray")
-        self.tableView.reloadData()
-      }
-      
-      alert.addTextField { (alertTextfield) in
-        alertTextfield.placeholder = "Create new Item"
-        textField = alertTextfield
-      }
-      
-      alert.addAction(alertAction)
-      
-      present(alert, animated: true, completion: nil)
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        saveItems()
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
     }
-  
-
+    
+    //MARK: - Actions
+    
+    @IBAction func addItemButtonTapped(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add new Todoey Item", message: "", preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            
+            guard textField.text?.isEmpty == false else {
+                return
+            }
+            
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
+            
+            self.saveItems()
+            
+        }
+        
+        alert.addTextField { (alertTextfield) in
+            alertTextfield.placeholder = "Create new Item"
+            textField = alertTextfield
+        }
+        
+        alert.addAction(alertAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    //MARK : - Save and Load items from a local .plist file
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Erreor encoding item array; \(error)")
+        }
+        
+        tableView.reloadData()
+    }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error while decoding item array, \(error)")
+            }
+        }
+    }
 }
---
+++ Todoey/Models/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Mehdi Chennoufi on 21/02/2018.
+//  Copyright © 2018 Mehdi Chennoufi. All rights reserved.
+//
+
+import Foundation
+
+class Item: Codable {
+    var title: String = ""
+    var done: Bool = false
+}
